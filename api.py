c56247b3ebf6bf10aca7c9e39d80b40e71f1ca18
gusavato <augusto.abad@outlook.com>
2023-09-03 17:09:12 +0200

    search_id and get_details ok

--- api.py
+++ api.py
@@ -1,6 +1,60 @@
 import requests
 from password import *
+import pandas as pd
+from datetime import datetime
 
 """
 Funciones consulta API TMDB
+
 """
+
+scan = pd.read_parquet('scan.parquet')
+
+headers = {"accept": "application/json", "Authorization": f"Bearer {TOKEN}"}
+
+
+def search_id(title, year):
+    """Función que devuelve el id de TMDB para un título"""
+
+    global headers
+
+    format_title = title.replace(' ', '%20')
+
+    url = f"https://api.themoviedb.org/3/search/movie?query={format_title}&include_adult=false&language=ES&page=1&year={year}"
+
+    response = requests.get(url, headers=headers).json()
+
+    return response['results'][0]['id']
+
+
+def get_details(TMDB_id):
+    """Función que devuelve los detalles de la pelicula"""
+
+    global headers
+
+    url = f"https://api.themoviedb.org/3/movie/{TMDB_id}?language=ES"
+
+    response = requests.get(url, headers=headers).json()
+
+    dictio = dict()
+    dictio['Titulo'] = response['title']
+    dictio['Titulo_Original'] = response['original_title']
+    dictio['Year'] = datetime.strptime(
+        response['release_date'], '%Y-%m-%d').year
+    dictio['Duracion'] = response['runtime']
+    dictio['Tag_Line'] = response['tagline']
+    dictio['Sinopsis'] = response['overview']
+    dictio['Genero'] = [gen['name'] for gen in response['genres']]
+    dictio['TMDB_rate'] = response['vote_average']
+    dictio['Poster'] = 'https://image.tmdb.org/t/p/w400' + \
+        response['poster_path']
+    dictio['Productoras'] = [prod['name']
+                             for prod in response['production_companies']]
+    dictio['Pais'] = [country['iso_3166_1']
+                      for country in response['production_countries']]
+    dictio['Fecha_Estreno'] = datetime.strptime(
+        response['release_date'], '%Y-%m-%d').strftime('%d-%m-%Y')
+    dictio['TMDB_id'] = response['id']
+    dictio['IMDB_id'] = response['imdb_id']
+
+    return dictio
