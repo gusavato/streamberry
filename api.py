2fe0c052c752c7c709dc59381c307b23b39f40c8
gusavato <augusto.abad@outlook.com>
2023-09-03 18:02:34 +0200

    get_cat,get_video, get_director

--- api.py
+++ api.py
@@ -16,8 +16,6 @@ headers = {"accept": "application/json", "Authorization": f"Bearer {TOKEN}"}
 def search_id(title, year):
     """Función que devuelve el id de TMDB para un título"""
 
-    global headers
-
     format_title = title.replace(' ', '%20')
 
     url = f"https://api.themoviedb.org/3/search/movie?query={format_title}&include_adult=false&language=ES&page=1&year={year}"
@@ -29,8 +27,6 @@ def search_id(title, year):
 
 def get_details(TMDB_id):
     """Función que devuelve los detalles de la pelicula"""
-
-    global headers
 
     url = f"https://api.themoviedb.org/3/movie/{TMDB_id}?language=ES"
 
@@ -58,3 +54,49 @@ def get_details(TMDB_id):
     dictio['IMDB_id'] = response['imdb_id']
 
     return dictio
+
+
+def get_cast(TMDB_id):
+    """Función que retorna los 8 primeros actores de una película"""
+
+    url = f"https://api.themoviedb.org/3/movie/{TMDB_id}/credits?language=ES"
+
+    response = requests.get(url, headers=headers).json()
+
+    lst = []
+
+    for i in range(min(8, len(response['cast']))):
+        dictio = dict()
+        dictio['Id'] = response['cast'][i]['id']
+        dictio['Nombre'] = response['cast'][i]['name']
+        dictio['Foto'] = 'https://image.tmdb.org/t/p/w200' + \
+            response['cast'][i]['profile_path']
+        lst.append(dictio)
+
+    return pd.DataFrame(lst)
+
+
+def get_director_writer(TMDB_id):
+
+    url = f"https://api.themoviedb.org/3/movie/{TMDB_id}/credits?language=ES"
+
+    response = requests.get(url, headers=headers).json()
+
+    df = pd.DataFrame(response)
+
+    dictio = dict()
+    dictio['Director'] = df[df.job == 'Director']['name'].to_list()
+    dictio['Guion'] = df[df.job == 'Screenplay']['name'].to_list()
+
+    return dictio
+
+
+def get_video(TMDB_id):
+
+    url = f"https://api.themoviedb.org/3/movie/{TMDB_id}/videos?language=ES"
+
+    response = requests.get(url, headers=headers).json()
+
+    video = 'https://www.youtube.com/watch?v=' + response['results'][0]['key']
+
+    return video
